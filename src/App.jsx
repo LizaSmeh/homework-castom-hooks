a3865d642617b1b56abdb32f708a8822542ee585
Elizaveta Smekhova <smekhovaelizaveta@gmail.com>
2025-07-13 19:38:16 +0300

    Четвертое задание

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useHover } from './hooks/useHover'
+import { useViewportSize } from './hooks/useViewportSize'
 import './App.css'
 
 function App() {
-  const { hovered, ref } = useHover();
+  const { height, width } = useViewportSize();
 
   return (
-    <div ref={ref}>
-      {hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
-    </div>
+    <>
+      Width: {width}, height: {height}
+    </>
   );
 }
 
